ad2e8c66d568057930e6d968030ad6f931b8acb4
ryandvl <contatoryandvl55@gmail.com>
2025-01-24 13:42:39 -0300

    feat: creating session

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,29 @@
+import { authConfig } from "@/configs/auth";
+import { AppError } from "@/utils/AppError";
 import { Request, Response } from "express";
+import { sign } from "jsonwebtoken";
 
 class SessionsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+    const { username, password } = request.body;
+
+    const fakeUser = {
+      id: 1,
+      username: "ryan",
+      password: "123456",
+    };
+
+    if (username !== fakeUser.username || password !== fakeUser.password) {
+      throw new AppError("Username or password invalid.", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({}, secret, {
+      expiresIn,
+      subject: String(fakeUser.id),
+    });
+
+    return response.json({ token });
   }
 }
 
